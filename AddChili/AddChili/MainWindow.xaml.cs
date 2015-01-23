0019101b2276a4b140f5895aaaf3395e84ef7129
Mattias Andersen <bazizten@yahoo.se>
2015-01-23 15:17:08 +0100

    Connected to database

--- AddChili/AddChili/MainWindow.xaml.cs
+++ AddChili/AddChili/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AddChiliDataBase;
 
 namespace AddChili
 {
@@ -20,9 +21,11 @@ namespace AddChili
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataBaseConnector dbo = new DataBaseConnector();
         public MainWindow()
         {
             InitializeComponent();
+            dbo.GetChili();
         }
 
         private void AddChiliButton1_Click(object sender, RoutedEventArgs e)
---
+++ AddChili/AddChiliDataBase/DataBaseConnector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddChiliDataBase
+{
+    public class DataBaseConnector
+    {
+        private const string connectionString = @"Server=.\SQLEXPRESS;Database=Chili;Trusted_Connection=yes;";
+        
+        public void GetChili()
+        {
+            string query = @"SELECT * FROM Chili.Chili";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                var result = command.ExecuteReader();
+
+                while (result.Read())
+                {
+                    var chilies = result["Name"];
+                }
+            }
+        }
+    }
+}
