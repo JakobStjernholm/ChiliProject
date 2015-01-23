860668aaecccfdf0764f41f64d004dec8f3d3310
Jakob <jakob.stjerna@gmail.com>
2015-01-23 14:43:10 +0100

    Here comes the project

---
+++ AddChili/AddChili/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AddChili
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddChiliButton1_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://chililovers.nu/");
+        }
+
+        private void ChiliColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
